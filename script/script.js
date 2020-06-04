e2d940acce31751835ec87a613097e09a98d1a5e
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-04 16:44:01 +0300

    feat: added tasks of the first lesson

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let money = 10;
+let income = 2000;
+let addExpenses = 1500;
+let deposit = 300;
+let mission  = 3000;
+let period = 30;
+
+alert('Это текст первого урока');
+
+console.log(mission);
