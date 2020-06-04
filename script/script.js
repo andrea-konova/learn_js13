1b258193ee2ed1f5b0a1451973fe89d03fd0d6bb
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-04 17:00:40 +0300

    fix: fixed variable values

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-let money = 10;
-let income = 2000;
-let addExpenses = 1500;
-let deposit = 300;
-let mission  = 3000;
-let period = 30;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission ;
+let period;
 
 alert('Это текст первого урока');
 
