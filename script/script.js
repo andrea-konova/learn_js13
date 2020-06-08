a8805b0d2bc61b81e598941bcbd2e9b690067a2d
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-08 17:45:21 +0300

    feat: completed a simple task from 7 to 11 points

--- script/script.js
+++ script/script.js
@@ -27,5 +27,19 @@ console.log(addExpenses);
 
 console.log('Бюджет на месяц ' + budgetMonth);
 
-let budgetDay = money / 30;
-console.log('Бюджет на день ' + budgetDay);
+let missionCompleted = Math.ceil(mission / budgetMonth);
+
+console.log('Цель будет достигнута за: ' + missionCompleted + ' месяцев');
+
+let budgetDay = Math.floor(budgetMonth / 30);
+console.log('Бюджет на день: ' + budgetDay);
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (600 <= budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (0 <= budgetDay < 600) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+  console.log('Что то пошло не так');
+}
