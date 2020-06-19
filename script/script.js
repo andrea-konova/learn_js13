9f5289b8e03aa84c94665467eb497b379742490e
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-19 18:08:40 +0300

    fix: fixed receiving list items

--- script/script.js
+++ script/script.js
@@ -6,16 +6,10 @@ const allBooks = document.querySelectorAll('.books'),
   bookTitle = document.getElementsByTagName('h2'),
   advertising = document.querySelector('.adv'),
   list = document.getElementsByTagName('ul'),
-  listItems = document.getElementsByTagName('li'),
+  listItems2 = list[0].getElementsByTagName('li'),
+  listItems5 = list[5].getElementsByTagName('li'),
+  listItems6 = list[2].getElementsByTagName('li'),
   newListItem = document.createElement('li');
-
-console.log(allBooks);
-console.log(book);
-console.log(body);
-console.log(bookTitle);
-console.log(list);
-console.log(listItems);
-console.log(newListItem);
 
 allBooks[0].prepend(book[1]);
 allBooks[0].append(book[2]);
@@ -28,16 +22,16 @@ bookTitle[2].style.color = "darkkhaki";
 
 advertising.remove();
 
-listItems[12].before(listItems[14]);
-listItems[16].before(listItems[8]);
-listItems[8].after(listItems[12]);
-listItems[9].after(listItems[12]);
+listItems2[1].after(listItems2[3]);
+listItems2[2].after(listItems2[6]);
+listItems2[3].after(listItems2[8]);
+listItems2[10].before(listItems2[5]);
 
-
-listItems[37].after(listItems[45]);
-listItems[43].before(listItems[39]);
-listItems[45].before(listItems[41]);
+listItems5[1].after(listItems5[9]);
+listItems5[2].after(listItems5[4]);
+listItems5[5].after(listItems5[4]);
+listItems5[9].before(listItems5[6]);
 
 newListItem.textContent = "Глава 8: За пределами ES6";
 list[5].append(newListItem); 
-listItems[57].after(listItems[56]);
+list[5].append(listItems6[9])
