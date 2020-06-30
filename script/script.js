c33141ca3cb8e751ea183fef84beb3a9a369c4e7
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-30 16:25:45 +0300

    feat: video passed

--- script/script.js
+++ script/script.js
@@ -1,6 +1,38 @@
 window.addEventListener('DOMContentLoaded', () => {
-  'use srtrict';
+	'use srtrict';
 
-	console.log(1);
+	// Timer
+	function countTimer(deadline) {
+		const timerHours = document.querySelector('#timer-hours'),
+			timerMinutes = document.querySelector('#timer-minutes'),
+			timerSeconds = document.querySelector('#timer-seconds');
+
+		function getTimeRemaining() {
+			const dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining / 60 / 60);
+			return { timeRemaining, hours, minutes, seconds };
+		}
+
+		function upDateClock() {
+			const timer = getTimeRemaining();
+
+			timerHours.textContent = timer.hours;
+			timerMinutes.textContent = timer.minutes;
+			timerSeconds.textContent = timer.seconds;
+
+			if (timer.timeRemaining > 0) {
+				setTimeout(upDateClock, 1000);
+			}
+		}
+
+		upDateClock();
+	}
+
+	countTimer('1 jule 2020');
+	// setInterval(countTimer, 1000, '1 jule 2020');
 
 });
