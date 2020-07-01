d54e80d77e68276a44d1559ecdd0716e4ace06af
Andrii Konovalenko <an256.ak@gmail.com>
2020-07-02 01:59:49 +0300

    fix: fixed third point lesson18 edit

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	'use srtrict';
 
 	// timer
-	const countTimer = (deadline) => {
+	const countTimer = deadline => {
 		const timerHours = document.querySelector('#timer-hours'),
 			timerMinutes = document.querySelector('#timer-minutes'),
 			timerSeconds = document.querySelector('#timer-seconds');
@@ -15,14 +15,14 @@ window.addEventListener('DOMContentLoaded', () => {
 				minutes = Math.floor((timeRemaining / 60) % 60),
 				hours = Math.floor(timeRemaining / 60 / 60);
 			return { timeRemaining, hours, minutes, seconds };
-    };
-    
-    const addZero = (num) => {
-      if (num < 10) {
+		};
+
+		const addZero = num => {
+			if (num < 10) {
 				return '0' + num;
-      }
-      return num;
-    };
+			}
+			return num;
+		};
 
 		const upDateClock = () => {
 			const timer = getTimeRemaining();
@@ -31,26 +31,17 @@ window.addEventListener('DOMContentLoaded', () => {
 			timerMinutes.textContent = addZero(timer.minutes);
 			timerSeconds.textContent = addZero(timer.seconds);
 
-      if (timer.timeRemaining <= 0) {
-        clearInterval(upDateClock);
-      }
-    };
-    
-		const timer = getTimeRemaining();
+			if (timer.timeRemaining <= 0) {
+				clearInterval(upDateClock);
+				console.log('Очистка setInterval');
+				timerHours.textContent = '00';
+				timerMinutes.textContent = '00';
+				timerSeconds.textContent = '00';
+			}
+		};
 
-		if (timer.timeRemaining > 0) {
-			let timerId = setInterval(upDateClock, 1000);
-      console.log('Вызов setInterval');
-      if (timer.timeRemaining <= 0) {
-        clearInterval(timerId);
-      }
-    }  
-    
-    //  else if (timer.timeRemaining <= 0) {
-		// 	timerHours.textContent = '00';
-		// 	timerMinutes.textContent = '00';
-		// 	timerSeconds.textContent = '00';
-		// }
+		setInterval(upDateClock, 1000);
+		console.log('Вызов setInterval');
 	};
 
 	countTimer('3 jule 2020');
