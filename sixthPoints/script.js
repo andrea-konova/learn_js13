48afe585d2f20bb2de33285e01ec949dcc8c327a
Andrii Konovalenko <an256.ak@gmail.com>
2020-07-03 13:40:02 +0300

    fix: fixed second task

--- sixthPoints/script.js
+++ sixthPoints/script.js
@@ -1,66 +1,81 @@
-'use strict';
 
-const date = new Date();
 
-const hours = date.getHours();
-if (hours >= 0 && hours <= 5) {
-	console.log('Доброй ночи');
-}
-if (hours > 5 && hours <= 11) {
-	console.log('Доброe утро');
-}
-if (hours > 11 && hours <= 17) {
-	console.log('Добрый день');
-}
-if (hours > 17 && hours <= 23) {
-	console.log('Добрый вечер');
-}
+const date = new Date(),
+	day = date.getDay(),
+	hours = date.getHours(),
+	minutes = date.getMinutes(),
+	seconds = date.getSeconds();
 
-const day = date.getDay();
-let dayWeek;
-switch (day) {
-case 0:
-	dayWeek = 'Воскресенье';
-	break;
-case 1:
-	dayWeek = 'Понедельник';
-	break;
-case 2:
-	dayWeek = 'Вторник';
-	break;
-case 3:
-	dayWeek = 'Среда';
-	break;
-case 4:
-	dayWeek = 'Четверг';
-	break;
-case 5:
-	dayWeek = 'Пятница';
-	break;
-case 6:
-	dayWeek = 'Суббота';
-	break;
-}
-console.log('Сегодня: ' + dayWeek);
+const getHi = () => {
+	let hello;
+	if (hours >= 0 && hours <= 5) {
+		hello = 'Доброй ночи';
+	}
+	if (hours > 5 && hours <= 11) {
+		hello = 'Доброe утро';
+	}
+	if (hours > 11 && hours <= 17) {
+		hello = 'Добрый день';
+	}
+	if (hours > 17 && hours <= 23) {
+		hello = 'Добрый вечер';
+  }
+  
+	const div = document.createElement('div');
+	div.innerHTML = hello;
+	document.body.append(div);
+};
 
-let meridiem;
-if (hours >= 0 && hours < 12) {
-	meridiem = 'AM';
-} else {
-	meridiem = 'PM';
-}
-console.log('Текущее время: ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + meridiem);
+const getDayWeek = () => {
 
-const newDay = new Date('1 january 2021');
-const msecondsToDay = 24 * 60 * 60 * 1000;
-const days = Math.floor((newDay - date) / msecondsToDay);
-let dayName;
-const lastNum = days.toString().slice(-1);
-if (lastNum === 1) {
-	dayName = 'день';
-} else if (lastNum > 1 && lastNum < 4) {
-	dayName = 'дня';
-} else {
-	dayName = 'дней';
-}
-console.log('До нового года осталось: ' + days + ' ' + dayName);
+	const dayWeek = [
+		'Воскресенье',
+		'Понедельник',
+		'Вторник',
+		'Среда',
+		'Четверг',
+		'Пятница',
+		'Суббота'
+	];
+
+	const div = document.createElement('div');
+	div.innerHTML = 'Сегодня: ' + dayWeek[day];
+	document.body.append(div);
+};
+
+const getMeridiem = () => {
+	let meridiem;
+	hours < 12 ? meridiem = 'AM' : meridiem = 'PM';
+
+	const div = document.createElement('div');
+	div.innerHTML = 'Текущее время: ' + hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
+	document.body.append(div);
+};
+
+const getNewYearDays = () => {
+	const newDay = new Date('1 january 2021');
+	const msecondsToDay = 24 * 60 * 60 * 1000;
+	const days = Math.floor((newDay - date) / msecondsToDay);
+	let dayName;
+	const lastNum = days.toString().slice(-1);
+	if (lastNum == 1) {
+		dayName = 'день';
+	} else if (lastNum > 1 && lastNum < 4) {
+		dayName = 'дня';
+	} else {
+		dayName = 'дней';
+	}
+
+	const div = document.createElement('div');
+	div.innerHTML = 'До нового года осталось: ' + days + ' ' + dayName;
+	document.body.append(div);
+};
+
+const start = () => {
+	getHi(hours);
+	getDayWeek(day);
+	getMeridiem(hours);
+	getNewYearDays();
+};
+
+start();
